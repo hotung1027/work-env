bb750be7ef0400ed6718995f64cec23f28d4efc0
TANG Ho Tung,Randy <hotung1027@gmail.com>
2021-03-10 07:24:05 +0800

    added invoke zsh to the script for install oh-my-zsh

--- init_workspace.sh
+++ init_workspace.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install curl wget git build-essential
-sudo cp ./.* $HOME
+sudo apt-get install -y curl wget git build-essential zsh
+sudo cp ./.* $HOME/
+zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sudo apt remove --purge vim
-sudo apt-get install python3-dev ruby-dev liblua5.3-dev lua5.3 libperl-dev perl libgtk-3-dev xorg-dev cscope
+sudo apt-get install -y python3-dev ruby-dev liblua5.3-dev lua5.3 libperl-dev perl libgtk-3-dev xorg-dev cscope
+mkdir -p ~/Downloads
+cd ~/Downloads	
 git clone https://github.com/vim/vim
+cd ~/Downloads/vim
 ./configure --with-features=huge --enable-python3interp --enable-rubyinterp --enable-luainterp --enable-perlinterp --enable-gui=gtk3 --enable-cscope --with-python3-command=python3 --prefix=/usr
 sudo make -j
 sudo make install -j
